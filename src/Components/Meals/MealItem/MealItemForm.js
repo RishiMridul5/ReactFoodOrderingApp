4483134a7752e4db022311dabb0edb21228bb38c
Mona Lisa <hrishipaul555@gmail.com>
2022-11-03 16:28:47 +0530

    Amount field validity implemented

--- src/Components/Meals/MealItem/MealItemForm.js
+++ src/Components/Meals/MealItem/MealItemForm.js
@@ -2,23 +2,33 @@ import style from "./MealItemForm.module.css";
 import Button from "../../UI/Button";
 import Input from "../../UI/Input";
 import { CartContext } from "../../../store/cart-ctx";
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 
 const MealItemForm = ({ mealData }) => {
   const [amount, setAmount] = useState(1);
-  const [amountInputValid, setAmountInputValid] = useState(true)
+  const [amountInputValid, setAmountInputValid] = useState(true);
   const { addItem } = useContext(CartContext);
 
   const handleAmountChange = (e) => {
     setAmount(e.target.value);
   };
+
+  // For amount input field validity
+  useEffect(() => {
+    if (+amount < 1 || +amount > 5) setAmountInputValid(false);
+    else setAmountInputValid(true);
+  }, [amount]);
+
   function submitHandler(e) {
     e.preventDefault();
-    const newItem = {
-      amount: amount,
-      ...mealData,
-    };
-    addItem(newItem); // add item to Cart
+
+    if (amountInputValid) {
+      const newItem = {
+        amount: amount,
+        ...mealData,
+      };
+      addItem(newItem); // add item to Cart
+    }
   }
 
   return (
@@ -29,13 +39,12 @@ const MealItemForm = ({ mealData }) => {
           onChange: handleAmountChange,
           type: "number",
           id: "amt",
-          min: "1",
-          max: "5",
           step: "1",
           defaultValue: "1",
         }}
       />
       <Button className="btn solid primary" />
+      {!amountInputValid ? <p>Enter a valid amount</p> : ""}
     </form>
   );
 };
